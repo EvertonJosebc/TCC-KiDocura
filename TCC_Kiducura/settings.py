babf20b05bc267e1a99dd43bc7276a6fb3902ab7
EvertonJosebc <everton.jose@escolar.ifrn.edu.br>
2024-11-06 20:55:34 -0300

    pequena alteração para vizualização no admin

--- TCC_Kiducura/settings.py
+++ TCC_Kiducura/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     
     'apps.users',
     'apps.partners',
-    'apps.production'
+    'apps.production',
 ]
 
 MIDDLEWARE = [
--- apps/production/admin.py
+++ apps/production/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Fruta, Compra, Producao, Entrega
 
 # Register your models here.
+
+admin.site.register(Fruta)
+admin.site.register(Compra)
+admin.site.register(Producao)
+admin.site.register(Entrega)
